c5837f5e2946cc5089205743213c1f8f56add937
mee <me>
2021-08-30 15:15:48 +0900

    make function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 use std::process::Command;
 use std::env;
 
-fn main() {
-    // Filesystem Size Used Avail Use% Mounted on
-    let args: Vec<String> = env::args().collect();
-    let system = &args[1];
-    let command = "df -H -t ".to_string() + system;
-    let mut spl: Vec<&str> = command.split(" ").collect();
+fn get_df_data(command:String) -> String {
+    let spl: Vec<&str> = command.split(" ").collect();
 
     let output = Command::new(spl[0])
         .args(&spl[1..])
@@ -15,15 +11,19 @@ fn main() {
 
     let out = output.stdout;
     let out_str = String::from_utf8_lossy(&out);
-    spl = out_str.split("\n").collect();
 
+    String::from(out_str)
+
+}
+
+fn str_molding(mut spl: Vec<&str>) -> Vec<String> {
     spl.remove(0);
     spl.remove(spl.len() - 1);
-    let mut string_data: Vec<String> = Vec::new();
+    let mut string_vec: Vec<String> = Vec::new();
 
     for disk_str in spl {
         let mut ss : String = String::from("");
-        let mut before:char = 'a';
+        let mut before:char = '\0';
 
         for s in disk_str.chars() {
             if s != ' ' {
@@ -37,9 +37,24 @@ fn main() {
             before = s;
 
         }
-        string_data.push(ss);
+        string_vec.push(ss);
     }
 
-    println!("{:?}", string_data);
+    string_vec
+
+}
+
+fn main() {
+    // Filesystem Size Used Avail Use% Mounted on
+    let args: Vec<String> = env::args().collect();
+    let system = &args[1];
+    let command = "df -H -t ".to_string() + system;
+
+    let out_str = get_df_data(command);
+    let spl: Vec<&str> =  out_str.split("\n").collect();
+
+    let string_vec = str_molding(spl);
+
+    println!("{:?}", string_vec);
 
 }
